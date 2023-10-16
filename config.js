2bf7a3d3884e9e4e0f2858d254733d18d051257a
YashGovindani <yashgovindani222@gmail.com>
2023-10-16 17:58:44 +0530

    updating port

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
         username: "pratilipi",
         password: "pratilipi",
         host: "127.0.0.1",
-        port: 5432,
+        port: 9022,
         database: "pratilipi_user_db"
     }
 }
